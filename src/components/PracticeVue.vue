438c97cd6358f4e623acdaee33367f2f465811cd
Soliloquiy <ibrahia@ucalgary.ca>
2021-10-11 00:03:39 +0000

    First Commit

---
+++ src/components/PracticeVue.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="Test">
+    <h1>{{ msg }}</h1>
+    <div> "changing works pretty fast here" </div>
+    <p>Job is: {{ job }}</p>
+    <p>Function call: {{ greet('Evening') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PracticeVue',
+  data () {
+    return {
+      msg: 'Beginning Practice',
+      job: 'Developer'
+    }
+  },
+  methods: {
+    greet: function (time) {
+      return `Good ${time} ${this.job}`
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
